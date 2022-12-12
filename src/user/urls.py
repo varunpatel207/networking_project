9ee4e4a7292c63322575a4b28f97678eafdc4868
Varun <varunpatel207@gmail.com>
2022-12-11 23:34:31 -0500

    - removed login url

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from user.views import UserViews, UserListCreateAPIView
+from user.views import UserViews
 
 app_name = "user"
 
 urlpatterns = [
     path('/all', UserViews.all, name='all'),
-    path('/login', UserViews.login, name='login'),
 ]
--- src/user/views.py
+++ src/user/views.py
@@ -7,13 +7,6 @@ from user.models import User
 from user.serializers import UserSerializer
 
 
-class UserListCreateAPIView(ListCreateAPIView):
-    throttle_scope = "datalist"
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    filterset_class = UserFilterSet
-
-
 class UserViews():
     @api_view(['GET'])
     def all(request):
@@ -23,13 +16,4 @@ class UserViews():
         if user_objects:
             for user_object in user_objects:
                 user_list.append(user_object)
-        return Response(user_list)
-
-    @api_view(['POST'])
-    def login(request):
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        user_list = []
-
         return Response(user_list)
